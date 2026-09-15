71aadf771411b4b2ec98001d86c5d330b10dab1d
Egor137-YaJ <egorsid777@icloud.com>
2026-09-15 21:39:44 +0300

    third task

--- main.c
+++ main.c
@@ -1,38 +1,22 @@
 
 #include <stdio.h>
 
-int max_of_3(int a, int b, int c) {
-    int max = a;
-    if (b > max) {
-        max = b;
-    } else if (c > max) {
-        max = c;
-    } else {
-        max = a;
+
+int factorial(int n) {
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res *= i;
     }
-    return max;
+    return res;
 }
 
 
 int main(void) {
     int x;
-    int y;
-    int z;
     scanf("%d", &x);
-    scanf("%d", &y);
-    scanf("%d", &z);
-    int max = max_of_3(x, y, z);
-    printf("maximum of %d, %d, %d is %d\n", x, y, z, max);
+    printf("factorial of %d is %d\n", x, factorial(x));
     return 0;
 }
-
-
-
-
-
-
-
-
 
 
 
